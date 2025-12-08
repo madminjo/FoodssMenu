82becc22fa0b9d4efd9b7abf31a04577e389f04f
ScrayMadaminjon <madazimovmadaminjonn@gmail.com>
2025-12-08 16:01:52 +0600

    feat: FullCode

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/basket' element={<Basket />} />
+				<Route path='/menu' element={<Home />} />
 			</Routes>
 		</AppContext.Provider>
 	)
